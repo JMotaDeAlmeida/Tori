68789348b9d2d0fa69f882393dce1636f02af5f8
João Mota de Almeida <joapedromota803@gmail.com>
2026-04-26 22:33:11 -0300

    Moved pomodoro commands to pomodor.py

---
+++ commands/pomodoro.py
@@ -0,0 +1,29 @@
+from discord.ext import commands
+import asyncio
+
+class Pomodoro(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def pomodoro(self, ctx, *args):
+        if len(args) != 2:
+            await ctx.send("Assim não funciona. Tenta colocar o periodo de estudo e descanso")
+            return
+        
+        try:
+            study = int(args[0])
+            breaks = int(args[1])
+        except ValueError:
+            await ctx.send("Insira números válidos")
+            return
+        
+        if study <= 0 or breaks <= 0:
+            await ctx.send("Valores precisam ser positivos")
+            return
+        
+        await ctx.send(f"Iniciando foco de {study} minutos e descanso de {breaks} minutos")
+        await asyncio.sleep(study*60)
+        await ctx.send(f"Foco acabou")
+        await asyncio.sleep(breaks*60)
+        await ctx.send(f"Descanso acabou")
--- main.py
+++ main.py
@@ -17,23 +17,5 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def ping(ctx):
     await ctx.send("Pong!")
 
-@bot.command()
-async def pomodoro(ctx, *args):
-    if len(args) != 2:
-        await ctx.send("Assim não funciona. Tenta colocar o periodo de estudo e descanso")
-        return
-
-    try:
-        study = int(args[0])
-        breaks = int(args[1])
-    except ValueError:
-        await ctx.send("Insira números válidos")
-        return
-
-    if study <= 0 or breaks <= 0:
-        await ctx.send("Valores precisam ser positivos")
-        return
-
-    await ctx.send(f"Iniciando foco de {study} minutos e descanso de {breaks} minutos")
 
 bot.run(DISCORD_TOKEN)
